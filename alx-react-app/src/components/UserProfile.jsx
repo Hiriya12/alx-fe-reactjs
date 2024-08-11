ba897c34f5df2a09bbfa971fc3aa0bf109b75607
Hiriya12 <hiriyadejene@gmail.com>
2024-08-11 17:03:31 +0300

    added Userprofile components

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,15 +1,11 @@
-function UserProfile() {
-    return (
+
         const UserProfile = (props) => {
             return (
               <div>
-                <h2>name: {props.name}</h2>
+                <h2>Name: {props.name}</h2>
                 <p>Age: {props.age}</p>
                 <p>Bio: {props.bio}</p>
               </div>
             );
-          };
-    );
-}
-
+};
 export default UserProfile;
