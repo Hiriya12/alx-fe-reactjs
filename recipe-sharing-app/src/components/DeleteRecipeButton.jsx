7f13ec45cf0df07b4734b37d90bcf20d3de9b0e1
Hiriya12 <hiriyadejene@gmail.com>
2024-08-25 23:01:58 +0300

    second commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -2,9 +2,10 @@ import { useRecipeStore } from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-
+  const navigate = useNavigate();
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/');
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
