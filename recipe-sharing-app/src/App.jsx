e68d1161f9b97e5ef8483ab0c42f1ce2e046455c
Hiriya12 <hiriyadejene@gmail.com>
2024-08-25 14:14:22 +0300

    Initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (  
+    <>
+   <AddRecipeForm></AddRecipeForm>
+   <RecipeList></RecipeList>
+   </>
+  );
+}
+
+export default App ;
