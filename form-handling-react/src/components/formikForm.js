b80f26289e049bb13817475a39d22346f5a0b985
Hiriya12 <hiriyadejene@gmail.com>
2024-09-01 20:15:14 +0300

    Updated commit

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,53 +2,44 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email format').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
 const FormikForm = () => {
-    const initialValues = {
-        username: '',
-        email: '',
-        password: ''
-    };
-
-    const validationSchema = Yup.object({
-        username: Yup.string().required('Username is required'),
-        email: Yup.string().email('Invalid email format').required('Email is required'),
-        password: Yup.string().required('Password is required')
-    });
-
-    const handleSubmit = (values, { resetForm }) => {
+  return (
+    <Formik
+      initialValues={{ username: '', email: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
         console.log('Form submitted:', values);
-        // Simulate API call here
         resetForm();
-    };
-
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-        >
-            {({ isSubmitting }) => (
-                <Form>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <Field type="text" name="username" id="username" />
-                        <ErrorMessage name="username" component="p" />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <Field type="email" name="email" id="email" />
-                        <ErrorMessage name="email" component="p" />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <Field type="password" name="password" id="password" />
-                        <ErrorMessage name="password" component="p" />
-                    </div>
-                    <button type="submit" disabled={isSubmitting}>Register</button>
-                </Form>
-            )}
-        </Formik>
-    );
+      }}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="p" />
+          </div>
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="p" />
+          </div>
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="p" />
+          </div>
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default FormikForm;
