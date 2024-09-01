5725d4189ba9d406feea6195bb7835318d55d12f
Hiriya12 <hiriyadejene@gmail.com>
2024-09-01 13:24:00 +0300

    updated commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,29 +1,50 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import React from 'react';
 import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails';
-import EditRecipeForm from './components/EditRecipeForm';
-import CreateRecipeForm from './components/CreateRecipeForm'; // This handles recipe creation
-import DeleteRecipeButton from './components/DeleteRecipeButton';
 import AddRecipeForm from './components/AddRecipeForm';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
+
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/create">Create Recipe</Link>
-            </li>
-          </ul>
-        </nav>
-        <FavoritesList />
-        <RecommendationsList />
-        <Routes>
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <Router>
+    //   <div>
+    //     <nav>
+    //       <ul>
+    //         <li>
+    //           <Link to="/">Home</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/create">Create Recipe</Link>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //     <FavoritesList />
+    //     <RecommendationsList />
+        /* <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/edit/:id" element={<EditRecipeForm />} />
@@ -32,8 +53,5 @@ const App = () => {
           <Route path='/add/:id' element={<AddRecipeForm />} />
         </Routes>
       </div>
-    </Router>
-  );
-};
+    </Router> */
 
-export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const recipes = useRecipeStore(state => state.recipes);
 
   return (
     <div>
-      {filteredRecipes.map(recipe => (
+      {recipes.map(recipe => (
         <div key={recipe.id}>
-          <Link to={`/recipe/${recipe.id}`}>
-            <h3>{recipe.title}</h3>
-          </Link>
+          <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
-          <p>Prep Time: {recipe.prepTime} minutes</p>
         </div>
       ))}
     </div>
   );
 };
-
 export default RecipeList;
