694248cf85b9b392ede5f125d935d2f51cbe94d5
Hiriya12 <hiriyadejene@gmail.com>
2024-09-01 21:35:21 +0300

    updated commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,36 +3,35 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Profile from './components/Profile';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
-import Blog from './components/Blog';
-import BlogPost from './components/BlogPost';
+import Blog from './components/Blog'; // Make sure to import the Blog component
+import BlogPost from './components/BlogPost'; // Import BlogPost component
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
+  const isAuthenticated = false; // Simulate authentication status
+
   return (
-    <AuthProvider>
-      <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/blog">Blog</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-          <Route
-            path="/profile/*"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/blog">Blog</Link> {/* Add Blog link */}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} /> {/* Base Blog route */}
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Blog post dynamic route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
